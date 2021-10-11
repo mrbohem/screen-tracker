0bcf004513e377d0c0086ba426a44d22d1a1b84a
Shivam <developer@siecindia.com>
2021-10-11 20:29:28 +0530

    wip

--- src/ScreenWireServiceProvider.php
+++ src/ScreenWireServiceProvider.php
@@ -23,12 +23,8 @@ class ScreenWireServiceProvider extends PackageServiceProvider
             true => \Mrbohem\ScreenWire\Service\AuthService::class,
         ];
 
-<<<<<<< HEAD
         $mainObj = $service[config('screen-wire.auth')] ?? null;
         if($mainObj){
-=======
-        if ($mainObj) {
->>>>>>> fb461ad6d8dcc2ceffda632eece7a415f61f0322
             (new MainService(new $mainObj()))->send();
         }
     }
